fbe6c86df2efcb94d02a20020114f2eadef52cfd
lakshmi0398 <lakshmiaviligonda30@gmail.com>
2024-03-03 15:33:08 +0000

    Message

---
+++ src/main/java/com/example/artgallery/ArtGalleryApplication.java
@@ -0,0 +1,11 @@
+package com.example.artgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtGalleryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArtGalleryApplication.class, args);
+    }
+}
